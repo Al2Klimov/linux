ff59535cc45ad1e1212ae1b7abb3f8241f2b0503
Alexander A. Klimov <grandmaster@al2klimov.de>
2020-04-27 06:37:00 +0200

    Replace HTTP links with HTTPS: www.openssl.org
    ... for security reasons.
    
    No breaking change as the HTTP vhost redirects to HTTPS.

--- arch/x86/crypto/poly1305-x86_64-cryptogams.pl
+++ arch/x86/crypto/poly1305-x86_64-cryptogams.pl
@@ -14,7 +14,7 @@
 # Written by Andy Polyakov <appro@openssl.org> for the OpenSSL
 # project. The module is, however, dual licensed under OpenSSL and
 # CRYPTOGAMS licenses depending on where you obtain it. For further
-# details see http://www.openssl.org/~appro/cryptogams/.
+# details see https://www.openssl.org/~appro/cryptogams/.
 # ====================================================================
 #
 # This module implements Poly1305 hash for x86_64.
