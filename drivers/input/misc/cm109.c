8412929bb787ea102089986a773558c684a80a63
Alexander A. Klimov <grandmaster@al2klimov.de>
2020-04-27 06:42:09 +0200

    Replace HTTP links with HTTPS: www.packetizer.com
    ... for security reasons.
    
    No breaking change as the HTTP vhost redirects to HTTPS.

--- drivers/input/misc/cm109.c
+++ drivers/input/misc/cm109.c
@@ -257,7 +257,7 @@ static unsigned short keymap_usbph01(int scancode)
 /*
  * Keymap for ATCom AU-100
  * https://www.atcom.cn/products.html 
- * http://www.packetizer.com/products/au100/
+ * https://www.packetizer.com/products/au100/
  * http://www.voip-info.org/wiki/view/AU-100
  *
  * Contributed by daniel@gimpelevich.san-francisco.ca.us
